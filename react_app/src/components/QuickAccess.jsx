d5b12b6efbcc8824102319f9f03c177fb8b99da9
eduardoogliari <eduardo.ogliari@outlook.com>
2024-10-08 00:58:09 -0300

    Configurado links na barra de acesso

--- react_app/src/components/QuickAccess.jsx
+++ react_app/src/components/QuickAccess.jsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
+
 function QuickAccess() {
     return (
         <span className="quick-access">
             <ul>
-                <li><a href="#">Plataformas</a></li>
-                <li><a href="#">Jogos</a></li>
-                <li><a href="#">Empresas</a></li>
+                {/* <li><a href="#">Plataformas</a></li> */}
+                <li><Link to="plataforma/">Plataformas</Link></li>
+                <li><Link to="search/">Jogos</Link></li>
+                <li><Link to="empresa/">Empresas</Link></li>
             </ul>
         </span>
     );
