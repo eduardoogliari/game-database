2c3a2803cae4c636506f834d1d1ca77ac1c52104
eduardoogliari <eduardo.ogliari@outlook.com>
2024-10-13 16:30:31 -0300

    Melhorado barra de navegação

--- react_app/src/components/QuickAccess.jsx
+++ react_app/src/components/QuickAccess.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 function QuickAccess() {
     return (
         <div className="quick-access">
+            <div></div>
             <ul>
                 {/* <li><a href="#">Plataformas</a></li> */}
                 <li><Link to="plataforma/">Plataformas</Link></li>
                 <li><Link to="search/">Jogos</Link></li>
                 <li><Link to="empresa/">Empresas</Link></li>
             </ul>
+            <div></div>
         </div>
     );
 };
